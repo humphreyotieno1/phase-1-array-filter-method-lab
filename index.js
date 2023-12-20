ed64e3d46ad925a78027f801f7e54bee6d8ead73
humphreyotieno1 <humphreyotieno04@gmail.com>
2023-12-20 17:05:31 +0300

    Filter function

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution here
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+
+function findMatching(drivers, findName) {
+    const newDriver = drivers.filter(driver => driver.toLowerCase() === findName.toLowerCase())
+    return newDriver
+}
+
+function fuzzyMatch(drivers, specifiedLetters) {
+    const newList = drivers.filter(driver => driver.toLowerCase().startsWith(specifiedLetters.toLowerCase()))
+    return newList
+}
+
+function matchName(drivers, target) {
+    const matchingName = drivers.filter(driver => driver.name === target)
+    return matchingName
+}
